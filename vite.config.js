fce5a5168bb81647f53e0abb19294f3ee32c3cea
yuna260 <162668777+yuna260@users.noreply.github.com>
2024-05-21 21:30:10 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,5 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   // needed for github pages just put the repo name here
-  base: "/d3-basic-scatterplot/",
+  base: "/d3-basic-scatterplot-main/",
 });
